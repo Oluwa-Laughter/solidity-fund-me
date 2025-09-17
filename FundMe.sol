fe9515b7dce399245e8f01b055bd12ddc17f5779
Oluwa-Laughter <ismakinde@gmail.com>
2025-09-17 14:32:17 +0100

    constructor and modifier

--- FundMe.sol
+++ FundMe.sol
@@ -8,15 +8,47 @@ contract FundMe {
 
     uint256 public minimumUsd = 5e18;
 
+
     address[] public funders;
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
 
     function fund() public payable  {
         require(msg.value.getConversionRate() >= minimumUsd, "You need to spend more ETH!");
         funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
+        addressToAmountFunded[msg.sender] +=  msg.value;
     }
 
-    // function withdraw() public {}
+    function withdraw() public onlyOwner {
+
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+
+        funders = new address[](0);
+
+        //transfer
+        payable(msg.sender).transfer(address(this).balance);
+
+        //send
+        bool sendSuccess= payable(msg.sender).send(address(this).balance);
+        require(sendSuccess, 'Send Failed');
+
+        //call
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, 'Call Failed');
+
+    }
+
+    modifier onlyOwner(){
+        require(msg.sender == owner, "Must be owner!");
+        _;
+    }
 
 }
